f4b3e2479b071404a4cb469e1eed6cd0c94553ba
andrewdefries <andrew.defries@gmail.com>
2014-09-17 05:05:47 +0000

    work on fig 1,2,3,5

---
+++ Figure3/Tox21_nnm/MakeMeChemicalTables.sh
@@ -0,0 +1,31 @@
+rm -r WorkHere
+
+mkdir WorkHere
+
+cp *.R WorkHere/
+
+cp Tox21.sdf WorkHere/
+
+cd WorkHere/
+
+echo "####################"
+echo "####################"
+echo "####################"
+echo "ChemmineR and OpenBabel are working on it, have a large coffee if your file is bigger than 1000 compounds"
+echo "####################"
+echo "####################"
+echo "####################"
+
+#R CMD BATCH CleanSDF.R
+
+R CMD BATCH hwriteMoreNow.R
+
+echo "####################"
+echo "####################"
+echo "####################"
+echo "Work is done, please inspect test.html for accuracy"
+echo "####################"
+echo "####################"
+echo "####################"
+
+google-chrome test.html
